27e86d97754aef2a9da3757136fd1b6dc2c2270e
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-05-28 22:58:43 +0200

    Remove namefree expressions

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,6 @@
 mod parsers;
 pub mod syntax;
 pub use parsers::*;
-mod lowering;
-pub use lowering::*;
 mod diagnostics;
-pub mod namefree;
 pub use diagnostics::*;
-mod reducing;
-pub use reducing::*;
 pub mod evaluation;
